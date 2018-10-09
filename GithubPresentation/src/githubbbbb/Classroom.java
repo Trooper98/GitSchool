183f20d3081c66c1b401e075fac63d0ef24eeaa8
Trooper98 <oliver.g.ryan@gmail.com>
2018-10-09 16:14:38 +0200

    added a main called Classroom

---
+++ GithubPresentation/src/githubbbbb/Classroom.java
@@ -0,0 +1,9 @@
+package githubbbbb;
+
+public class Classroom {
+	public static void main(String [] args) {
+		SimpleCalculator calc = new SimpleCalculator();
+		System.out.println("the result is \n" + calc.add(1, 1));
+	}
+
+}
