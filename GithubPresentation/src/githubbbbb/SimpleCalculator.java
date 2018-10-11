1b51aea48969ccc8051898aa48ed388d9c563d5f
Gagandeep Singh <43956612+gagans0723@users.noreply.github.com>
2018-10-11 13:29:45 +0200

    refactored code in SimpleCalculator and update .gitignore

--- GithubPresentation/src/githubbbbb/SimpleCalculator.java
+++ GithubPresentation/src/githubbbbb/SimpleCalculator.java
@@ -2,37 +2,29 @@ package githubbbbb;
 
 public class SimpleCalculator implements CalculatorInterface{
 	//create calculator
-	public SimpleCalculator() {
-		
-	}
+	public SimpleCalculator() {}
 
 	@Override
 	public int add(int num1, int num2) {
 		// TODO Auto-generated method stub
-		int result = num1 + num2;
-		return result;
+		return num1 + num2;
 	}
 
 	@Override
 	public int subtract(int num1, int num2) {
 		// TODO Auto-generated method stub
-		int result = num1 - num2;
-		return result;
+		return num1 - num2;
 	}
 
 	@Override
 	public int divide(int num1, int num2) {
 		// TODO Auto-generated method stub
-		int result = num1 / num2;
-		return result;
+		return num1 / num2;
 	}
 
 	@Override
 	public int multiply(int num1, int num2) {
 		// TODO Auto-generated method stub
-		int result = num1 * num2;
-		return result;
+		return num1 * num2;
 	}
-	
-	
 }
