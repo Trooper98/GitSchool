d2fbc18e5e7e19ff8020d02153c56c153db31bfb
Amirneutron <43997168+Amirneutron@users.noreply.github.com>
2018-10-11 14:13:24 +0200

    Modulo funtion added

--- GithubPresentation/src/githubbbbb/Classroom.java
+++ GithubPresentation/src/githubbbbb/Classroom.java
@@ -3,6 +3,8 @@ package githubbbbb;
 public class Classroom {
 	public static void main(String [] args) {
 		SimpleCalculator calc = new SimpleCalculator();
+		
+		System.out.println(" === Welcome to Ranim Calculator === ");
 		System.out.println("the result is \n" + calc.add(1, 1));
 	}
 
--- GithubPresentation/src/githubbbbb/SimpleCalculator.java
+++ GithubPresentation/src/githubbbbb/SimpleCalculator.java
@@ -33,6 +33,11 @@ public class SimpleCalculator implements CalculatorInterface{
 		int result = num1 * num2;
 		return result;
 	}
+	@Override
+	public int modulo(int num1, int num2) {
+		// TODO Auto-generated method stub
+		int result = num1 % num2;
+		return result;
 	
 	
 }
