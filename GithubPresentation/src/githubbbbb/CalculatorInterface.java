362549c64fcde6fc0a403bdf066d0c1dbd00f754
Amirneutron <43997168+Amirneutron@users.noreply.github.com>
2018-10-26 11:02:23 +0200

    I added the modulo and square function int the interface class

--- GithubPresentation/src/githubbbbb/CalculatorInterface.java
+++ GithubPresentation/src/githubbbbb/CalculatorInterface.java
@@ -6,4 +6,6 @@ public interface CalculatorInterface {
 	int subtract(int num1, int num2);
 	int divide(int num1, int num2);
 	int multiply(int num1, int num2);
+	int modulo(int num1, int num2);
+	int square(int num1, int num2);
 }
